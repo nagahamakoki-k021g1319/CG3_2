ca9ead0ae3c1e8386652fdfd52a208de706f52bd
nagahama_kouki <k021g1319@g.neec.ac.jp>
2023-01-16 15:21:24 +0900

    プロジェクト ファイルを追加します。

---
+++ Resources/shaders/OBJPixelShader.hlsl
@@ -0,0 +1,12 @@
+#include "OBJShaderHeader.hlsli"
+
+Texture2D<float4> tex : register(t0);  // 0番スロットに設定されたテクスチャ
+SamplerState smp : register(s0);      // 0番スロットに設定されたサンプラー
+
+float4 main(VSOutput input) :sv_TARGET
+{
+	//テクスチャマッピング
+	float4 texcolor = tex.Sample(smp,input.uv);
+	//シェーディングによる色で描画
+	return input.color * texcolor;
+}
